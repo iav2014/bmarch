18820f5cbc3210554f4739d44dc0f5c495111b71
Ignacio Ariza Victoria <ignacio.ariza.victoria@everis.com>
2019-01-29 10:04:05 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+**banca march partial solution**
+
+Design Choices
+
+ Contingency solution for march. 
+ 
+ This code consists of an pdf read and extract information to process
+ .txt file
+ 
+
+(c) Nacho Ariza 2019
---
+++ app.js
@@ -0,0 +1,18 @@
+/*
+* goal: read pdf file
+* tenant: Banca Mech partial solution
+* author: (c) 29/01/2019 Nacho Ariza
+* MIT license
+*/
+
+let fs = require('fs');
+let PDFParser = require("pdf2json");
+let pdfParser = new PDFParser(this, 1);
+
+pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError)
+);
+pdfParser.on("pdfParser_dataReady", pdfData => {
+	fs.writeFileSync("./target/informe-diario-de-inversion17690.txt", pdfParser.getRawTextContent());
+});
+
+pdfParser.loadPDF("./source/informe-diario-de-inversion17690.pdf");
